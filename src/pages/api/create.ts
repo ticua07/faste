6280bb211a489b2537c362be1dc021888bc4bd0f
ticua07 <ticua@ticua.ar>
2024-03-10 17:45:48 -0300

    Add api/create/ route

---
+++ src/pages/api/create.ts
@@ -0,0 +1,25 @@
+import type { APIRoute } from "astro"
+import { prisma } from "../../../utils/prisma";
+import { z } from "zod";
+import { customAlphabet } from 'nanoid/non-secure'
+
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 12)
+
+
+export const POST: APIRoute = async ({ url, request }) => {
+    if (request.headers.get("Content-Type") === "application/json") {
+        const body = await request.json();
+        const content = body.content
+        if (z.string().min(1).safeParse(content).success) {
+            const randomLink = nanoid()
+            await prisma.paste.create({
+                data: {
+                    content,
+                    slug: randomLink
+                }
+            })
+            return new Response(JSON.stringify({ link: `${url}${randomLink}` }))
+        }
+    }
+    return new Response(JSON.stringify({ "success": false }))
+}
